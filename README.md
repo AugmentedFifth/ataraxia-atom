0ea32ed245b80a58ba13b8490e2fbbf552909688
Augmented Fifth <zcomito@gmail.com>
2018-04-23 23:26:43 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# language-ataraxia
+
+language support for the ataraxia programming language
+
+## features
+
+- syntax highlighting
+- that's it
+- what did you expect
+- a language server or something?
---
+++ spec/language-ataraxia-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import LanguageAtaraxiaView from '../lib/language-ataraxia-view';
+
+describe('LanguageAtaraxiaView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
